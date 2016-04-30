dbb5221c140383d6277da832ea872ba4086d07be
Wilfried Jeanniard <wilfried@willou.com>
2016-04-30 17:29:15 +0200

    add reference

--- metadata.rb
+++ metadata.rb
@@ -4,4 +4,6 @@ maintainer_email 'willou.com@gmail.com'
 license          'All rights reserved'
 description      'Installs/Configures RTM for OVH Servers'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1'
+source_url 'https://github.com/willoucom/chef-rtm-ovh'
+issues_url 'https://github.com/willoucom/chef-rtm-ovh/issues'
